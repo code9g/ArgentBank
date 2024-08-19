d677dc66549705d8e5bb3f6daf419aa247c35586
Pierre HENRY <pierre.henry@code99.fr>
2024-08-19 04:47:25 +0200

    Ajout image et mise en place header/footer (simple version)

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -8,11 +8,13 @@ import "./App.css";
 import Layout from "./layouts/Layout";
 import Error from "./pages/Error";
 import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
 
 const routes = createRoutesFromElements(
   <Route path="/" element={<Layout />} errorElement={<Error />}>
     <Route errorElement={<Error />}>
       <Route index element={<Home />} />
+      <Route path="sign-in" element={<SignIn />} />
     </Route>
   </Route>
 );
--- front/src/layouts/Header.jsx
+++ front/src/layouts/Header.jsx
@@ -1,5 +1,25 @@
+import { Link, NavLink } from "react-router-dom";
+import logo from "../assets/img/argentBankLogo.png";
+
 function Header() {
-  return <header>Header</header>;
+  return (
+    <nav className="main-nav">
+      <Link className="main-nav-logo" to="/">
+        <img
+          className="main-nav-logo-image"
+          src={logo}
+          alt="Argent Bank Logo"
+        />
+        <h1 className="sr-only">Argent Bank</h1>
+      </Link>
+      <div>
+        <NavLink className="main-nav-item" to="/sign-in">
+          <i className="fa fa-user-circle"></i>
+          Sign In
+        </NavLink>
+      </div>
+    </nav>
+  );
 }
 
 export default Header;
