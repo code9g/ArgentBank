32fc456c8d888a39daa821c198bf0ccc9c531e98
Pierre HENRY <pierre.henry@code99.fr>
2024-08-19 04:35:13 +0200

    Mise en place du routage et de gh-pages

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import "./App.css";
+import Layout from "./layouts/Layout";
+import Error from "./pages/Error";
+import Home from "./pages/Home";
+
+const routes = createRoutesFromElements(
+  <Route path="/" element={<Layout />} errorElement={<Error />}>
+    <Route errorElement={<Error />}>
+      <Route index element={<Home />} />
+    </Route>
+  </Route>
+);
+
+const router = createBrowserRouter(routes, { basename: "/ArgentBank" });
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
---
+++ front/src/pages/Error.jsx
@@ -0,0 +1,9 @@
+import { useRouteError } from "react-router-dom";
+
+function Error() {
+  const error = useRouteError();
+  console.error(error);
+  return <div>{error.statusText || "???"}</div>;
+}
+
+export default Error;
