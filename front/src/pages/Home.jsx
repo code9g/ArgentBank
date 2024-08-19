e217344456e1e6738d710c0dde33adfb225fd300
Pierre HENRY <pierre.henry@code99.fr>
2024-08-19 05:28:40 +0200

    Ajout composant Hero

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import iconChat from "../assets/img/icon-chat.png";
 import iconMoney from "../assets/img/icon-money.png";
 import iconSecurity from "../assets/img/icon-security.png";
 import Feature from "../components/Feature";
+import Hero from "../components/Hero";
 
 function Home() {
   const features = [
@@ -29,15 +30,7 @@ function Home() {
   ];
   return (
     <main>
-      <div className="hero">
-        <section className="hero-content">
-          <h2 className="sr-only">Promoted Content</h2>
-          <p className="subtitle">No fees.</p>
-          <p className="subtitle">No minimum deposit.</p>
-          <p className="subtitle">High interest rates.</p>
-          <p className="text">Open a savings account with Argent Bank today!</p>
-        </section>
-      </div>
+      <Hero />
       <section className="features">
         <h2 className="sr-only">Features</h2>
         {features.map((feature, index) => (
