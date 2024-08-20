e064b5292a0c4897cd25a4ab6e463ab8cd18d1ea
Pierre HENRY <pierre.henry@code99.fr>
2024-08-20 09:49:03 +0200

    page-error css

--- front/src/pages/Error.jsx
+++ front/src/pages/Error.jsx
@@ -3,9 +3,12 @@ import { useRouteError } from "react-router-dom";
 function Error() {
   const error = useRouteError();
 
-  console.error(error);
-
-  return <div>{error.statusText || "???"}</div>;
+  return (
+    <main className="page-error">
+      <h2>{error.status || error.code}</h2>
+      <p>{error.statusText || error.message}</p>
+    </main>
+  );
 }
 
 export default Error;
