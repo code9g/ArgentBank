b7068e777f9ef1b175f06f9af0e3e33d91af2e42
Pierre HENRY <pierre.henry@code99.fr>
2024-08-20 06:11:31 +0200

    ajout documentation

--- front/src/redux/store.js
+++ front/src/redux/store.js
@@ -2,6 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import { loginSlice } from "./slices/loginSlice";
 import { profileSlice } from "./slices/profileSlice";
 
+/**
+ * Constante du "store" de l'application
+ *
+ * @type {*}
+ */
 const store = configureStore({
   reducer: {
     login: loginSlice.reducer,
