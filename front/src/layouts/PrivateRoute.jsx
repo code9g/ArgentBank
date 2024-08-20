27bd2192cd64172f639cf0eeec9afabc3ae4d958
Pierre HENRY <pierre.henry@code99.fr>
2024-08-20 08:49:51 +0200

    ajout propriété "to"

--- front/src/layouts/PrivateRoute.jsx
+++ front/src/layouts/PrivateRoute.jsx
@@ -1,9 +1,14 @@
+import PropTypes from "prop-types";
 import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 
-function PrivateRoute() {
+function PrivateRoute({ to }) {
   const isAuth = useSelector((state) => state.login.token !== null);
-  return isAuth ? <Outlet /> : <Navigate to="/sign-in" replace={true} />;
+  return isAuth ? <Outlet /> : <Navigate to={to} replace={true} />;
 }
+
+PrivateRoute.propTypes = {
+  to: PropTypes.string,
+};
 
 export default PrivateRoute;
