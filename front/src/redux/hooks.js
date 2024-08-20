a0e7f865be9a7f3feabf96811bbfa3900fd1a9ea
Pierre HENRY <pierre.henry@code99.fr>
2024-08-20 09:05:25 +0200

    simplification

--- front/src/redux/hooks.js
+++ front/src/redux/hooks.js
@@ -4,13 +4,9 @@ const loginSelector = (state) => state.login;
 
 const profileSelector = (state) => state.profile;
 
-const isAuthSelector = (state) => state.login.token !== null;
-
 export const useLoginSelector = () => {
   const selector = useSelector(loginSelector);
   return { ...selector, isAuth: selector.token !== null };
 };
 
 export const useProfileSelector = () => useSelector(profileSelector);
-
-export const useIsAuthSelector = () => useSelector(isAuthSelector);
