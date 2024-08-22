b9c00dee5f828e49d9514e1745f1daf8b2376849
Pierre HENRY <pierre.henry@code99.fr>
2024-08-22 07:55:03 +0200

    Ajout onchange

--- front/src/components/forms/InputText.jsx
+++ front/src/components/forms/InputText.jsx
@@ -1,10 +1,10 @@
 import PropTypes from "prop-types";
 
-function InputText({ id, label, required = false }) {
+function InputText({ id, label, required = false, onChange = null }) {
   return (
     <div className="input-text">
       <label htmlFor={id}>{label}</label>
-      <input type="text" id={id} required={required} />
+      <input type="text" id={id} required={required} onChange={onChange} />
     </div>
   );
 }
@@ -13,6 +13,7 @@ InputText.propTypes = {
   id: PropTypes.string,
   label: PropTypes.string,
   required: PropTypes.bool,
+  onChange: PropTypes.func,
 };
 
 export default InputText;
