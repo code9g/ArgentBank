b3dbd297d80bcc297c7c403aae6420650c55e9b5
Pierre HENRY <pierre.henry@code99.fr>
2024-08-22 08:33:39 +0200

    résolution du toast x 2

--- front/src/pages/SignOut.jsx
+++ front/src/pages/SignOut.jsx
@@ -4,21 +4,35 @@ import { Navigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { signOut } from "../redux/actions";
 import { useAuthSelector } from "../redux/hooks";
+import { toastify } from "../utils/functions";
 
+let flag = false;
 function SignOut() {
   const { isAuth } = useAuthSelector();
   const dispatch = useDispatch();
 
   useEffect(() => {
-    if (isAuth) {
+    // Trick pour gérer le "Strict Mode" de react en mode développement
+    if (isAuth && (import.meta.env.PROD || flag)) {
+      const id = toast.loading("Disconnecting...");
       dispatch(signOut())
         .then(() => {
-          toast.success("You are logged out");
+          toast.update(
+            id,
+            toastify({ render: "You are logged out", type: "success" })
+          );
         })
-        .catch(() => {
-          toast.error("Wrong !");
+        .catch((error) => {
+          toast.update(
+            id,
+            toastify({
+              render: error.statusText || error.message,
+              type: "error",
+            })
+          );
         });
     }
+    return () => (flag = !flag);
   }, [isAuth, dispatch]);
 
   return <Navigate to="/" />;
