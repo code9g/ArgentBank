8ec9e4b6f5be3d8175c81d1e8b4ff118b7d987be
Pierre HENRY <pierre.henry@code99.fr>
2024-08-25 05:24:46 +0200

    correction jsdoc

--- front/src/redux/selectors.js
+++ front/src/redux/selectors.js
@@ -8,6 +8,7 @@ export const authSelector = (state) => state.auth;
 
 /**
  * Fonction d'extraction du state du profil de l'utilisateur
+ *
  * @param {Object} state Le state dont on veut récupérer les informations de l'utilisateur
  * @returns {Object}
  */
